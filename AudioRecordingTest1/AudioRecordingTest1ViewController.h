0877f03f4d062907e030456215887b9c3720a660
Akash Krishnan <ak@aakay.net>
2011-07-21 17:01:58 -0700

    Initial Commit
    This is the first commit. Adding AudioRecordingTest1 program (the first
    program)

--- AudioRecordingTest1/AudioRecordingTest1ViewController.h
+++ AudioRecordingTest1/AudioRecordingTest1ViewController.h
@@ -7,9 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreAudio/CoreAudioTypes.h>
 
-@interface AudioRecordingTest1ViewController : UIViewController {
+@interface AudioRecordingTest1ViewController : UIViewController <UIAlertViewDelegate, AVAudioRecorderDelegate>
+{
+    IBOutlet UIButton *recordAudioButton;
+    IBOutlet UIButton *playRecordedAudioButton;
+    IBOutlet UIActivityIndicatorView *recordAudioActivitySpinner;
+    bool recordAudioToggle;
     
+    NSURL *recordedAudioTmpFile;
+    AVAudioRecorder *audioRecorder;
+    NSError *recordAudioError;
 }
+
+@property (nonatomic, retain) UIButton *recordAudioButton;
+@property (nonatomic, retain) UIButton *playRecordedAudioButton;
+@property (nonatomic, retain) UIActivityIndicatorView *recordAudioActivitySpinner;
+
+-(IBAction)recordAudioButtonPressed:(id)sender;
+-(IBAction)playRecordedAudioButtonPressed:(id)sender;
 
 @end
--- AudioRecordingTest1/AudioRecordingTest1ViewController.m
+++ AudioRecordingTest1/AudioRecordingTest1ViewController.m
@@ -10,8 +10,82 @@
 
 @implementation AudioRecordingTest1ViewController
 
+@synthesize recordAudioButton;
+@synthesize playRecordedAudioButton;
+@synthesize recordAudioActivitySpinner;
+
+-(IBAction)recordAudioButtonPressed:(id)sender
+{
+    if(!recordAudioToggle)
+    {
+        UIAlertView *recordAudioAlertView = [[UIAlertView alloc] initWithTitle:@"Record Audio"
+                                                                       message:@"You are about to record audio. Would you like to proceed?"
+                                                                      delegate:self
+                                                             cancelButtonTitle:@"No"
+                                                             otherButtonTitles:@"Yes", nil];
+        recordAudioAlertView.tag = 1;
+        [recordAudioAlertView show];
+        [recordAudioAlertView release];
+    }
+    else
+    {
+        // Stop recording now
+        
+        recordAudioToggle = false;
+        [playRecordedAudioButton setEnabled:true];
+        [recordAudioButton setTitle:@"Record Audio" forState:UIControlStateNormal];
+        [recordAudioActivitySpinner stopAnimating];
+        
+        [audioRecorder stop];
+    }
+}
+
+-(IBAction)playRecordedAudioButtonPressed:(id)sender
+{
+    AVAudioPlayer * avPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:recordedAudioTmpFile error:&recordAudioError];
+	[avPlayer prepareToPlay];
+	[avPlayer play];
+}
+
+-(void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    switch(actionSheet.tag)
+    {
+        case 1:
+        {
+            if(buttonIndex == 1)
+            {
+                // start recording audio now
+                
+                recordAudioToggle = true;
+                [playRecordedAudioButton setEnabled:false];
+                [recordAudioButton setTitle:@"Stop Recording" forState:UIControlStateNormal];
+                [recordAudioActivitySpinner startAnimating];
+                
+                NSMutableDictionary *recordAudioSettings = [[NSMutableDictionary alloc] init];
+                [recordAudioSettings setValue:[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey:AVFormatIDKey];
+                [recordAudioSettings setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+                [recordAudioSettings setValue:[NSNumber numberWithInt:2] forKey:AVNumberOfChannelsKey];
+                
+                recordedAudioTmpFile = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"%.0f.%@", [NSDate timeIntervalSinceReferenceDate] * 1000.0, @"caf"]]];
+                
+                audioRecorder = [[AVAudioRecorder alloc] initWithURL:recordedAudioTmpFile settings:recordAudioSettings error:&recordAudioError];
+                [audioRecorder setDelegate:self];
+                [audioRecorder prepareToRecord];
+                [audioRecorder record];
+                
+                // In order to record for set interval, use following:
+                //[recorder recordForDuration:(NSTimeInterval) 10];
+            }
+        }
+    }
+}
+
 - (void)dealloc
 {
+    [recordAudioButton release];
+    [playRecordedAudioButton release];
+    [recordAudioActivitySpinner release];
     [super dealloc];
 }
 
@@ -25,19 +99,34 @@
 
 #pragma mark - View lifecycle
 
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    recordAudioToggle = false;
+    [playRecordedAudioButton setEnabled:false];
+    [recordAudioActivitySpinner setHidesWhenStopped:true];
+    
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&recordAudioError];
+    [audioSession setActive:true error:&recordAudioError];
 }
-*/
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+    
+    recordAudioToggle = false;
+    [playRecordedAudioButton setEnabled:false];
+    
+    NSFileManager *fm = [NSFileManager defaultManager];
+	[fm removeItemAtPath:[recordedAudioTmpFile path] error:&recordAudioError];
+    recordedAudioTmpFile = nil;
+	[fm dealloc];
+    
+	[audioRecorder dealloc];
+    audioRecorder = nil;
+    recordAudioError = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
